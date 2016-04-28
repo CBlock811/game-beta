0e87c279f42803cddd48d56a3edd7fd42a6ec965
Christian <christian.block@yahoo.com>
2016-04-27 18:11:46 -0700

    player movement, boundaries, and map

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,25 @@
 class Player
 
   attr_accessor :speed
+  attr_accessor :x
+  attr_accessor :y
+  attr_accessor :sprite
 
   def initialize(speed = 3)
+    @sprite = Gosu::Image.new("graphics/smiley.gif")
     @speed = speed
+    @x = @y = 0
+  end
+
+  def move(direction)
+    @x -= speed if direction == :left
+    @x += speed if direction == :right
+    @y -= speed if direction == :up
+    @y += speed if direction == :down
+  end
+
+  def draw
+    @sprite.draw(@x, @y, 0)
   end
 
 end
