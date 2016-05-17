cca1d96cb92e494b8bdb045c77155db7c09cdc46
Brendan Saunders <brendan@taskrabbit.com>
2016-05-16 21:10:39 -0700

    implement placeholder health system

--- lib/game.rb
+++ lib/game.rb
@@ -14,28 +14,44 @@ class Game < Gosu::Window
 
   def button_down(id)
     close if id == Gosu::KbEscape
+
+      @player.take_damage if id == Gosu::KbT
+      @player.heal_damage if id == Gosu::KbY
+    
   end
 
   def update
-    if button_down?(Gosu::KbLeft)
-      @player.move(:left) unless @player.x <= PIT_OFFSET
-    end
+    if @player.alive?
+      if button_down?(Gosu::KbLeft)
+        @player.move(:left) unless @player.x <= PIT_OFFSET
+      end
 
-    if button_down?(Gosu::KbRight)
-      @player.move(:right) unless @player.x >= (self.width - @player.sprite.width - (@player.speed + PIT_OFFSET))
-    end
+      if button_down?(Gosu::KbRight)
+        @player.move(:right) unless @player.x >= (self.width - @player.sprite.width - (@player.speed + PIT_OFFSET))
+      end
 
-    if button_down?(Gosu::KbUp)
-      @player.move(:up) unless @player.y <= PIT_OFFSET
-    end
+      if button_down?(Gosu::KbUp)
+        @player.move(:up) unless @player.y <= PIT_OFFSET
+      end
 
-    if button_down?(Gosu::KbDown)
-      @player.move(:down) unless @player.y >= (self.height - @player.sprite.height - (@player.speed + PIT_OFFSET))
+      if button_down?(Gosu::KbDown)
+        @player.move(:down) unless @player.y >= (self.height - @player.sprite.height - (@player.speed + PIT_OFFSET))
+      end
     end
   end
 
   def draw
     @map.draw(0, 0)
     @player.draw
+
+    health_text = Gosu::Image.from_text("Health: #{@player.health}", 32)
+    health_text.draw(0, 0, 0, 1, 1, Gosu::Color::BLACK)
+
+    if !@player.alive?
+      game_over_text = Gosu::Image.from_text("YOU DEDED!", 64, {align: :center})
+      text_x = (self.width/2) - game_over_text.width/2
+      text_y = (self.height/2) - game_over_text.height/2
+      game_over_text.draw(text_x, text_y, 1, 1, 1, Gosu::Color::BLACK)
+    end
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,15 @@
 class Player
 
   attr_accessor :speed, :x, :y, :sprite
+  attr_accessor :max_health, :health, :damage
 
   PIT_OFFSET = 96
 
   def initialize(speed = 4)
     @sprite = Gosu::Image.new("graphics/smiley.gif")
     @speed = speed
+    @max_health = @health = 6
+    @damage = 1
     @x = @y = PIT_OFFSET
   end
 
@@ -15,6 +18,19 @@ class Player
     @x += speed if direction == :right
     @y -= speed if direction == :up
     @y += speed if direction == :down
+  end
+
+  def take_damage(amount = 1)
+    @health -= amount
+  end
+
+  def heal_damage(amount = 1)
+    @health += amount
+    @health = @max_health if @health >= @max_health
+  end
+
+  def alive?
+    @health > 0
   end
 
   def draw
